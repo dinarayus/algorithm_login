2b51465a33be866449abfa4af4cd61c66510ec82
Dinara <dinarakhubiyeva@gmail.com>
2022-06-04 16:16:32 +0700

    Commit Changes

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def check_login_email(string):
+    """
+    Если result == True, то удовлетворяет
+    Если result == False, то не удовлетворяет
+    :param string:
+    :return: result = {True, False}
+    """
+    items = ['*', '\\', '|', '/', '-', ',', ':', '_', ';', "'", '@', '&', ')', '(', '{', '}', '[', ']', '=', '?']
+    result = True
+
+    for item in items:
+        if item in string:
+            result = False
+            break
+    return result
 
 def exit_program(string):
     if string == 'exit':
@@ -5,34 +20,71 @@ def exit_program(string):
     else:
         return True
 
-def registr_func():
-    pass
+def registr_func(string):
+    run1 = True
+    
+    while run1:
+        print(" enter your login: ")
+        login = input()
+        print("enter your email:")
+        email = input()
+        print("enter your password:")
+        password = input()
+    
+        if email not in emails and '@' in email:
+            if login not in logins:
+                if check_login_email(login):
+                    index = email.index('@')
+                    if check_login_email(email[:index] + email[index+1:]):
+                        logins.append(login)
+                        passwords.append(password)
+                        emails.append(email)
+                        print('Вы успешно зарегистрировались')
+                        run1 = False
+                    else:
+                        print('Неверный формат почты')
+                else:
+                    print('Неверный формат логина')
+            else:
+                print("Этот логин уже занят.Введите другой логин")
+        else:
+            print("Почта уже занятия или не подходит")
+    return True
 
-def login_func():
-    print(" enter your login: ")
-    login = input()
-    print("enter your password:")
-    password = input()
+def login_func(string):
+    run1 = True
 
-    if login in logins:
-        index = logins.index(login)
-        if password == passwords[index]:
-            print("Вы вошли в систему")
-            print(emails[index])
+    while run1:
+        print(" enter your login: ")
+        login = input()
+        print("enter your password:")
+        password = input()
+
+        if login in logins:
+            index = logins.index(login)
+            if password == passwords[index]:
+                print("Вы вошли в систему")
+                print(emails[index])
+                print("Хотите выйти[exit]?")
+                string = input()
+                run1 = exit_program(string)
+
+            else:
+                print("Логин или пароль неверный")
+                print("Хотите повторить[yes] или выйти[exit]?")
+                string = input()
+                run1 = exit_program(string)
         else:
             print("Логин или пароль неверный")
             print("Хотите повторить[yes] или выйти[exit]?")
             string = input()
-            return exit_program(string)
-    else:
-        print("Логин или пароль неверный")
-        print("Хотите повторить[yes] или выйти[exit]?")
-        string = input()
-        return exit_program(string)
+            run1 = exit_program(string)
+
+    return True  # run == False
 
 
 if __name__ == '__main__':
-
+  #  wrong_elements = ['`', '~', '!', '#', '№',  '%', '^', '&', '*', '(',  ')', '-', '_', "'", ',', '?', '"', '|', ';', ':', '+']
     commands = ['L', 'R', 'exit']
     logins = ['dinarayus', "kaseke", "azik"]
     passwords = ["12345", "67890", "qwerty"]
@@ -53,9 +105,11 @@ if __name__ == '__main__':
 
         if string in commands:
             if string == 'L':
-                run = login_func()
+                run = login_func(string)
+
             elif string == 'R':
-                pass
+                run = registr_func(string)
+
             else:
                 run = exit_program('exit')
 

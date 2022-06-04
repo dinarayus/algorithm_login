1f59b6b3c19eb5e27fd8ed605d9254f2fe23b5b0
Dinara <dinarakhubiyeva@gmail.com>
2022-06-04 10:38:36 +0700

    Commit Changes

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+if __name__ == '__main__':
+
+    logins = ['dinarayus', "kaseke", "azik"]
+    passwords = ["12345", "67890", "qwerty"]
+    emails = ["dinara@gmail.com", "kaseke@gmail.com", "azik@gmail.com"]
+
+
+
+# if "*" or "/" "\ "  "|" "&"  "+" "-" ":" ";"  "," "_"
